a8259e8cfb2681fe5644042ffdee766bba60a74b
RizwanAmjad <rizwanamjadms@gmail.com>
2021-12-29 11:26:47 +0500

    Created Project

---
+++ README.md
@@ -0,0 +1,6 @@
+# MongoDB Assignment
+
+# Contributors Group Members
+1. Rizwan Amjad
+2. Muhammad Ahsan
+3. Kashoo
---
+++ app/src/main/java/com/rizwanamjadnov/mongodbatlas/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rizwanamjadnov.mongodbatlas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
