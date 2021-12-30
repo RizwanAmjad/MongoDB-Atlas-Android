f8759c7567b154f93ea28e8c6613637118153ac0
kashir <hasnainkashir6105@gmail.com>
2021-12-30 10:06:30 +0500

    Call Create Function

--- app/src/main/java/com/rizwanamjadnov/mongodbatlas/MainActivity.kt
+++ app/src/main/java/com/rizwanamjadnov/mongodbatlas/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.rizwanamjadnov.mongodbatlas
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.rizwanamjadnov.mongodbatlas.mongodb.Person
 
 
 class MainActivity : AppCompatActivity() {
-
+lateinit var create_Button:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        create_Button=findViewById(R.id.createButton)
+        create_Button.setOnClickListener{
+            val cls2 = Person("kashir",16)
+            cls2.create("kashir",16)
+            val intent= Intent(this,Person::class.java)
+            startActivity(intent)
+        }
     }
 }
